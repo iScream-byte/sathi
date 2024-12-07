431a0f664d03c8900e939980f7258b031a3ad78c
iScream-byte <suvojit.pal2@live.com>
2024-12-07 11:48:42 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sathi',
+  webDir: 'www'
+};
+
+export default config;
